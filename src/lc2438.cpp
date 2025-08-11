28440957527dfdd0a3e45a949862a173c3e99f07
Adrian <adrian.lin.1.0.0@gmail.com>
2025-08-11 14:46:07 +0800

    lint lc2438.cpp

--- src/lc2438.cpp
+++ src/lc2438.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 class Solution {
-public:
+ public:
     vector<int> productQueries(int n, const vector<vector<int>>& queries) {
+        constexpr int mod = 1e9+7;
         vector<int> powers;
-        for (int i = 0; i < 31; i++){
-            if (((n >> i) & 1) == 1) {
-                powers.push_back(1 << i);
+        for (int i = 0, p2 = 1; i < 31; ++i, p2 <<= 1) {
+            if ((p2 & n) == p2) {
+                powers.push_back(p2);
             }
         }
         vector<int> res;
-        for (auto query: queries){
-            long long ans = 1;
-            for (int i = query[0]; i <= query[1];i++){
-                ans =  (ans * powers[i])% mod;
+        for (const auto& query : queries) {
+            int64_t ans = 1;
+            for (int i = query[0]; i <= query[1]; ++i) {
+                ans = (ans * powers[i])% mod;
             }
             res.push_back(ans);
         }
         return res;
     }
-private:
-    static constexpr int mod = 1e9+7;
 };
